5ba6cd21521e09b9290d7b2a4a08eb8c9c144e16
lfyuomr-gylo <lfyuomr-gylo@yandex.ru>
2022-01-30 16:43:41 +0300

    Add test request to Anki.

--- popup.js
+++ popup.js
@@ -4,11 +4,36 @@ chrome.tabs.query({
     currentWindow: true
 }, function (tabs) {
     // and use that tab to fill in out title and url
-    var tab = tabs[0];
+    const tab = tabs[0];
 
-    let testParagraph = document.getElementById("testParagraph");
+    const testParagraph = document.getElementById("testParagraph");
     if (tab.url.includes("spanishdict.com")) {
         testParagraph.textContent += "This is spanishdict.com: yes. ";
     }
-    testParagraph.textContent += "Tab title: '" + tab.title + "'.";
+    // testParagraph.textContent += "Tab title: '" + tab.title + "'.";
+
+    // call AnkiConnect
+    fetch("http://localhost:8765", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "action": "getNumCardsReviewedToday",
+            "version": 6
+        })
+    }).then(async (resp) => {
+        if (resp.status !== 200) {
+            return Promise.reject(new Error(resp.statusText))
+        }
+        const body = await resp.json()
+        if (body.error) {
+            return Promise.reject(new Error(body.error))
+        }
+
+        testParagraph.textContent += "Anki Request status: " + resp.status + ". "
+        testParagraph.textContent += "Cards reviewed today in Anki: " + body.result + "."
+    }).catch(function (err) {
+        console.error(err)
+    })
 });
