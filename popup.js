a92fd57df058dca38d493756d22c809005dde053
lfyuomr-gylo <lfyuomr-gylo@yandex.ru>
2022-01-30 17:28:07 +0300

    Add sample spanishdict request.

--- popup.js
+++ popup.js
@@ -1,3 +1,24 @@
+const doFetch = ({method, url, queryParams, headers, body}) => {
+    if (!method) {
+        method = (body === undefined) ? "GET" : "POST"
+    }
+    if (body !== undefined && typeof body !== "string") {
+        body = JSON.stringify(body)
+    }
+
+    url = new URL(url)
+    if (queryParams) {
+        Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]))
+    }
+
+    return fetch(url, {
+        method,
+        headers,
+        body,
+        mode: "no-cors",
+    })
+}
+
 // Get the current active tab in the lastly focused window
 chrome.tabs.query({
     active: true,
@@ -10,18 +31,18 @@ chrome.tabs.query({
     if (tab.url.includes("spanishdict.com")) {
         testParagraph.textContent += "This is spanishdict.com: yes. ";
     }
-    // testParagraph.textContent += "Tab title: '" + tab.title + "'.";
 
     // call AnkiConnect
-    fetch("http://localhost:8765", {
+    doFetch({
         method: "POST",
+        url: "http://localhost:8765",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
+        body: {
             "action": "getNumCardsReviewedToday",
             "version": 6
-        })
+        }
     }).then(async (resp) => {
         if (resp.status !== 200) {
             return Promise.reject(new Error(resp.statusText))
@@ -35,5 +56,33 @@ chrome.tabs.query({
         testParagraph.textContent += "Cards reviewed today in Anki: " + body.result + "."
     }).catch(function (err) {
         console.error(err)
+    })
+
+    // obtain word usage examples from spanishdict
+    doFetch({
+        method: "GET",
+        url: "https://examples1.spanishdict.com/explore",
+        queryParams: {
+            lang: "es",
+            q: "leer",
+            numExplorationSentences: 100,
+        }
+    }).then(async (resp) => {
+        if (resp.status !== 200) {
+            console.info("Got response. Status code: " + resp.status)
+            return Promise.reject(new Error(resp.statusText))
+        }
+
+        const body = await resp.json();
+        console.log("got response from spanishdict", body)
+        body.data?.sentences?.forEach((sentence, idx) => {
+            if (idx > 0) {
+                return
+            }
+
+            testParagraph.textContent += "Sample sentence from spanishdict: " + sentence.source;
+        })
+    }).catch((err) => {
+        console.error(err);
     })
 });
